9a5a43a29179aced8ea8b8765c3b4dc93936e000
Daymond Blair <daymond.blair@gmail.com>
2018-08-10 18:25:42 -0700

    About page

--- dist/main.js
+++ dist/main.js
@@ -9,7 +9,10 @@ const navItems = document.querySelectorAll('.nav-item');
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+/*ROADBLOCK - get funcionality of toggleMenu (button click) to Work - 7/27/18
+  FIX - instead of addEventListener("click", toggleMenu()); use addEventListener("click", toggleMenu); - 8/10/18 */
 
+// TODO reverse list item slide in order
 function toggleMenu() {
     if (!showMenu) {
         menuBtn.classList.add('close');
@@ -17,6 +20,7 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
+        //navItems.slice().reverse().forEach(item => item.classList.add('show'));
 
         //Set Menu State
         showMenu = true;
@@ -26,10 +30,12 @@ function toggleMenu() {
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
+        //navItems.slice().reverse().forEach(item => item.classList.remove('show'));
 
         //Set Menu State
         showMenu = false;
     }
-    /*hit a wall - GET http://127.0.0.1:3000/dist/js/main.js/ 404 (Not Found)
-    (index):1 Refused to execute script from 'http://127.0.0.1:3000/dist/js/main.js/' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.*/
+    /*ROADBLOCK - GET http://127.0.0.1:3000/dist/js/main.js/ 404 (Not Found)
+    (index):1 Refused to execute script from 'http://127.0.0.1:3000/dist/js/main.js/' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled. - 8/8/2018
+      FIX - move main.js out of js folder into dist with index.html 8/10/2018*/
 }
