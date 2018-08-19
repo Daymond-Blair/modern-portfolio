eb757e5fcc9363bd40bcb6ad2a4695b5dc83c98c
Daymond Blair <daymond.blair@gmail.com>
2018-08-19 04:52:47 -0700

    Finished about me text, fixed js errors and footer

--- dist/main.js
+++ dist/main.js
@@ -4,27 +4,14 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-//const bioImage = document.querySelectorAll('.bio-image');
 
 // Set Initial State Of Menu
 let showMenu = false;
-//let hiddenPortrait = false;
+/*ROADBLOCK - errors thrown from toggleImage() when not on "about" page - 8/17/18
+FIX - wrapped toggleImage() and toggleMenu() into a new function menuClick() - 8/19/18*/
+menuBtn.addEventListener("click", menuClick);
 
 
-menuBtn.addEventListener("click", toggleMenu);
-menuBtn.addEventListener("click", hideImage);
-
-/*if (!hiddenPortrait) {
-    hideImage();
-
-    hiddenPortrait = true;
-}
-
-if (hiddenPortrait) {
-    showImage();
-
-    hiddenPortrait = false;
-}*/
 /*ROADBLOCK - get funcionality of toggleMenu (button click) to Work - 7/27/18
   FIX - instead of addEventListener("click", toggleMenu()); use addEventListener("click", toggleMenu); - 8/10/18 */
 
@@ -33,51 +20,41 @@ function toggleMenu() {
 
     if (!showMenu) {
         menuBtn.classList.add('close');
-        //bioImage.classList.add('show');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-        //navItems.slice().reverse().forEach(item => item.classList.add('show'));
 
         //Set Menu State
         showMenu = true;
     } else {
         menuBtn.classList.remove('close');
-        //bioImage.classLIst.remove('show');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
         navItems.forEach(item => item.classList.remove('show'));
-        //navItems.slice().reverse().forEach(item => item.classList.remove('show'));
+
 
         //Set Menu State
         showMenu = false;
     }
     /*ROADBLOCK - GET http://127.0.0.1:3000/dist/js/main.js/ 404 (Not Found)
     (index):1 Refused to execute script from 'http://127.0.0.1:3000/dist/js/main.js/' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled. - 8/8/2018
-      FIX - move main.js out of js folder into dist with index.html - 8/10/2018*/
+      WORKAROUND - moved main.js out of js folder into dist with index.html - 8/10/2018*/
 }
 
 function menuClick() {
-    menuBtn.addEventListener("click", toggleMenu);
-    //menuBtn.addEventListener("click", hideImage);
+    toggleMenu();
+    if (document.URL.indexOf("about.html") >= 0) {
+        toggleImage();
+    }
 }
 
-function hideImage() {
+function toggleImage() {
     let hide = document.getElementById("myPortrait");
     if (hide.style.visibility === "hidden") {
         hide.style.visibility = "visible";
     } else {
         hide.style.visibility = "hidden";
     }
-}
-
-function showImage() {
-    document.getElementById('btn').onclick = function() {
-        document.getElementById('myPortrait').className = "display";
-    }
-
-    document.getElementById('myPortrait').style.removeProperty('hide');
-
 }
